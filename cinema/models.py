c05f588f9905e60191d42089cbdd9eb438700cdc
Alexandr Politov <apolitov83@gmail.com>
2024-09-27 17:55:37 +0300

    flake8 mistakes

--- cinema/models.py
+++ cinema/models.py
@@ -6,7 +6,7 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=50)
 
     class Meta:
-        ordering=("last_name",)
+        ordering = ("last_name",)
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
