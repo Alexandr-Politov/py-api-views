3bccbd84d1c4eeced9bb1a1578b9b87bee765bcf
Alexandr Politov <apolitov83@gmail.com>
2024-09-29 21:37:28 +0300

    Commented-out code deleted

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -33,38 +33,3 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ("id", "title", "description", "actors", "genres", "duration")
-
-# class MovieSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(max_length=255)
-#     description = serializers.CharField()
-#     duration = serializers.IntegerField()
-#     actors = serializers.PrimaryKeyRelatedField(
-#         many=True, queryset=Actor.objects.all()
-#     )
-#     genres = serializers.PrimaryKeyRelatedField(
-#         many=True, queryset=Genre.objects.all()
-#     )
-#
-#     def create(self, validated_data):
-#         actors = validated_data.pop("actors")
-#         genres = validated_data.pop("genres")
-#         movie = Movie.objects.create(**validated_data)
-#         movie.actors.set(actors)
-#         movie.genres.set(genres)
-#         return movie
-#
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get("title", instance.title)
-#         instance.description = validated_data.get(
-#             "description", instance.description
-#         )
-#         instance.duration = validated_data.get("duration", instance.duration)
-#         if "actors" in validated_data:
-#             actors = validated_data.pop("actors")
-#             instance.actors.set(actors)
-#         if "genres" in validated_data:
-#             genres = validated_data.pop("genres")
-#             instance.genres.set(genres)
-#         instance.save()
-#         return instance
