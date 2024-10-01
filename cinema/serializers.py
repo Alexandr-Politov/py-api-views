008cfb6609f677b0e0ac7a71e4e3f96d5faf1ef5
Alexandr Politov <apolitov83@gmail.com>
2024-10-01 17:41:48 +0300

    serializers changed back into serializers.Serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,25 +4,65 @@ from rest_framework.validators import UniqueValidator
 from cinema.models import Actor, Genre, CinemaHall, Movie
 
 
-class ActorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Actor
-        fields = ("id", "first_name", "last_name")
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name
+        )
+        instance.save()
+        return instance
 
 
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Genre
-        fields = ("id", "name",)
+class GenreSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        max_length=50,
+        validators=[UniqueValidator(queryset=Genre.objects.all())]
+    )
+
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
 
 
-class CinemaHallSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CinemaHall
-        fields = ("id", "name", "rows", "seats_in_row")
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row
+        )
+        instance.save()
+        return instance
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
     actors = serializers.PrimaryKeyRelatedField(
         many=True, queryset=Actor.objects.all()
     )
@@ -30,6 +70,25 @@ class MovieSerializer(serializers.ModelSerializer):
         many=True, queryset=Genre.objects.all()
     )
 
-    class Meta:
-        model = Movie
-        fields = ("id", "title", "description", "actors", "genres", "duration")
+    def create(self, validated_data):
+        actors = validated_data.pop("actors")
+        genres = validated_data.pop("genres")
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors)
+        movie.genres.set(genres)
+        return movie
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description", instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
+        if "actors" in validated_data:
+            actors = validated_data.pop("actors")
+            instance.actors.set(actors)
+        if "genres" in validated_data:
+            genres = validated_data.pop("genres")
+            instance.genres.set(genres)
+        instance.save()
+        return instance
